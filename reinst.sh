af6b9aef8dfb9dd4a4747532a0f898645dc4d58f
Kartikaya Gupta <kgupta@mozilla.com>
2011-12-19 11:03:21 -0500

    Update reinst script to install from current dir or specified file; also force uninstall to pick up browser.js changes

--- reinst.sh
+++ reinst.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
-# adb uninstall org.mozilla.fennec_$USER
-adb install -r $HOME/zspace/builds/fennec-*.en-US.android-arm.apk
+FILE=${1-"./fennec-11.0a1.en-US.android-arm.apk"}
+if [ ! -f "$FILE" ]; then
+    FILE="$HOME/zspace/builds/fennec-11.0a1.en-US.android-arm.apk"
+    echo "Falling back to $FILE" > /dev/stderr
+    if [ ! -f "$FILE" ]; then
+        echo "Error: fallback $FILE not found!" > /dev/stderr
+        exit 1;
+    fi
+else
+    echo "Installing $FILE" > /dev/stderr
+fi
+
+adb uninstall org.mozilla.fennec_$USER
+adb install $FILE
