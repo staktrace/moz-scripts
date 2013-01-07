efa970cded14d5201eb692d642d9bd0b32c128d2
Kartikaya Gupta <kgupta@mozilla.com>
2013-01-07 15:49:00 -0500

    Add version upgrade check to reinst.sh

--- reinst.sh
+++ reinst.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-FILE=${1-"./fennec-20.0a1.en-US.android-arm.apk"}
+VERSION=20
+FILE=${1-"./fennec-$VERSION.0a1.en-US.android-arm.apk"}
 if [ ! -f "$FILE" ]; then
     FILE=$(ls -t1 $HOME/zspace/builds/fennec-*.apk | head -1)
     echo "Falling back to $FILE" > /dev/stderr
     if [ ! -f "$FILE" ]; then
         echo "Error: fallback $FILE not found!" > /dev/stderr
         exit 1;
+    fi
+    FOUND=${FILE##*/}
+    if [ ${FOUND:0:9} != "fennec-$VERSION" ]; then
+        echo "Warning: found ${FOUND:0:9}; this doesn't match expected version of fennec-$VERSION; $0 may need to be updated!"
     fi
 else
     echo "Installing $FILE" > /dev/stderr
