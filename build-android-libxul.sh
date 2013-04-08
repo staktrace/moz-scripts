26de7bd81c1dcc513bae6c1b237761c1da762503
Kartikaya Gupta <kgupta@mozilla.com>
2013-04-08 12:34:53 -0400

    Add scripts for rebuilding libxul

---
+++ build-android-libxul.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ ! -f "client.mk" ]; then
+    echo "Error: no client.mk found; you might be running in the wrong directory!" > /dev/stderr
+    exit 1;
+fi
+
+if [ ! -d "obj-android" ]; then
+    mkdir obj-android
+fi;
+
+(make -C obj-android/toolkit/library libs \
+    && make -C obj-android package \
+    && cp obj-android/dist/fennec*.apk $HOME/zspace/builds/
+) 2>&1 | tee obj-android/build.log
+
+exit ${PIPESTATUS[0]}
