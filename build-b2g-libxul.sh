7807ab68d60238dc8b922b70eb64d34c9b745603
Kartikaya Gupta <kgupta@mozilla.com>
2013-04-23 15:22:25 -0400

    Add zbl to build libxul for b2g

---
+++ build-b2g-libxul.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ ! -f "client.mk" ]; then
+    echo "Error: no client.mk found; you might be running in the wrong directory!" > /dev/stderr
+    exit 1;
+fi
+
+if [ ! -d "obj-gonk" ]; then
+    echo "Error: no obj-gonk found!" > /dev/stderr
+fi
+
+(make -C obj-gonk/toolkit/library libs \
+    && make -C obj-gonk package
+) 2>&1 | tee obj-gonk/build.log
+
+exit ${PIPESTATUS[0]}
