fd2dc844e828cb78330cbf0ef17ce6b306cb3926
Kartikaya Gupta <kgupta@mozilla.com>
2013-12-12 16:01:13 -0500

    Version bump

--- reinst.sh
+++ reinst.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=28
+VERSION=29
 FILE=${1-"./fennec-$VERSION.0a1.en-US.android-arm.apk"}
 if [ ! -f "$FILE" ]; then
     FILE=$(ls -t1 $HOME/zspace/builds/fennec-*.apk | head -1)
