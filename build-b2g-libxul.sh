8cf4871419714f4976d8f16e15ebf854172763b0
Kartikaya Gupta <kgupta@mozilla.com>
2014-06-13 15:28:05 -0400

    Move zbl2 to zbl

--- build-b2g-libxul.sh
+++ build-b2g-libxul.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
-if [ ! -f "client.mk" ]; then
-    echo "Error: no client.mk found; you might be running in the wrong directory!" > /dev/stderr
-    exit 1;
+if [ ! -d "objdir-gecko" ]; then
+    echo "Error: no objdir-gecko found!" > /dev/stderr
 fi
 
-if [ ! -d "obj-gonk" ]; then
-    echo "Error: no obj-gonk found!" > /dev/stderr
-fi
-
-(make -C obj-gonk/toolkit/library libs \
-    && make -C obj-gonk package
-) 2>&1 | tee obj-gonk/build.log
+(make -C objdir-gecko/toolkit/library libs \
+    && make -C objdir-gecko package
+) 2>&1 | tee objdir-gecko/build.log
 RET=${PIPESTATUS[0]}
 
 exit $RET
