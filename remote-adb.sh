418acefedd37325268269e2a59b12ea909fb6d9f
Kartikaya Gupta <kgupta@mozilla.com>
2015-01-14 16:00:51 -0500

    Export FORCE_GDB_PORT over ssh

--- remote-adb.sh
+++ remote-adb.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 echo "export ANDROID_ADB_SERVER_PORT=5038"
+echo "export FORCE_GDB_PORT=5039"
--- unremote-adb.sh
+++ unremote-adb.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 echo "export ANDROID_ADB_SERVER_PORT=5037"
+echo "export FORCE_GDB_PORT="
