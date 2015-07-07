c7b7e861eaaeb8cb7e9f5c2cc6bace191727c8b0
Kartikaya Gupta <kgupta@mozilla.com>
2015-07-07 16:15:15 -0400

    Update vcslookup to use new API

--- vcslookup.sh
+++ vcslookup.sh
@@ -3,5 +3,5 @@
 ssh people "grep $1 gecko-mapfile"
 if [ $? -ne 0 ]; then
     echo "Re-pulling mapfile..." > /dev/stderr
-    ssh people "rm gecko-mapfile && wget --quiet http://people.mozilla.org/~pmoore/vcs2vcs/gecko-dev/gecko-mapfile && grep $1 gecko-mapfile"
+    ssh people "rm gecko-mapfile && wget -O gecko-mapfile --quiet https://api.pub.build.mozilla.org/mapper/gecko-dev/mapfile/full && grep $1 gecko-mapfile"
 fi
