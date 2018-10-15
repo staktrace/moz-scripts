bf08dad301e12f11ce2e93c45ed10bc1f42131ee
Kartikaya Gupta <kgupta@mozilla.com>
2018-10-15 12:28:19 -0400

    Update vcslookup

--- vcslookup.sh
+++ vcslookup.sh
@@ -4,5 +4,5 @@ MAPFILE=$HOME/Downloads/gecko-mapfile
 grep $1 "$MAPFILE"
 if [ $? -ne 0 ]; then
     echo "Re-pulling mapfile..." > /dev/stderr
-    rm "$MAPFILE" && wget -O "$MAPFILE" --quiet https://api.pub.build.mozilla.org/mapper/gecko-dev/mapfile/full && grep $1 "$MAPFILE"
+    rm -f "$MAPFILE" && wget -O "$MAPFILE" --quiet https://mapper.mozilla-releng.net/gecko-dev/mapfile/since/2018-01-01 && grep $1 "$MAPFILE"
 fi
